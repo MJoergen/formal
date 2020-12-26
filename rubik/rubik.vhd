9e14bf9539bbf84e8d78442da7550ee3e685ca7a
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2020-12-26 18:06:24 +0100

    Rubik: Add formal verification

--- rubik/rubik.vhd
+++ rubik/rubik.vhd
@@ -31,7 +31,7 @@ use ieee.numeric_std.all;
 -- 1111 : U-
 -- Only these combinations of input pins are allowed
 --
--- The output signal done_o is tru when
+-- The output signal done_o is true when
 -- Ux = 0
 -- Fx = 1
 -- Rx = 2
@@ -39,7 +39,7 @@ use ieee.numeric_std.all;
 -- Bx = 4
 -- Lx = 5
 --
--- The input grid after reset is
+-- The input grid after reset is (TBD)
 -- Ux = x+0 mod 6
 -- Fx = x+1 mod 6
 -- Rx = x+2 mod 6
@@ -53,41 +53,67 @@ entity rubik is
       rst_i  : in  std_logic;
 
       cmd_i  : in  std_logic_vector(3 downto 0);
+
+      u0_o   : out std_logic_vector(2 downto 0);
+      u1_o   : out std_logic_vector(2 downto 0);
+      u2_o   : out std_logic_vector(2 downto 0);
+      u3_o   : out std_logic_vector(2 downto 0);
+      f0_o   : out std_logic_vector(2 downto 0);
+      f1_o   : out std_logic_vector(2 downto 0);
+      f2_o   : out std_logic_vector(2 downto 0);
+      f3_o   : out std_logic_vector(2 downto 0);
+      r0_o   : out std_logic_vector(2 downto 0);
+      r1_o   : out std_logic_vector(2 downto 0);
+      r2_o   : out std_logic_vector(2 downto 0);
+      r3_o   : out std_logic_vector(2 downto 0);
+      d0_o   : out std_logic_vector(2 downto 0);
+      d1_o   : out std_logic_vector(2 downto 0);
+      d2_o   : out std_logic_vector(2 downto 0);
+      d3_o   : out std_logic_vector(2 downto 0);
+      b0_o   : out std_logic_vector(2 downto 0);
+      b1_o   : out std_logic_vector(2 downto 0);
+      b2_o   : out std_logic_vector(2 downto 0);
+      b3_o   : out std_logic_vector(2 downto 0);
+      l0_o   : out std_logic_vector(2 downto 0);
+      l1_o   : out std_logic_vector(2 downto 0);
+      l2_o   : out std_logic_vector(2 downto 0);
+      l3_o   : out std_logic_vector(2 downto 0);
+
       done_o : out std_logic
    );
 end entity rubik;
 
 architecture synthesis of rubik is
 
-   signal u0 : std_logic_vector(2 downto 0);
-   signal u1 : std_logic_vector(2 downto 0);
-   signal u2 : std_logic_vector(2 downto 0);
-   signal u3 : std_logic_vector(2 downto 0);
+   signal u0 : std_logic_vector(2 downto 0) := (others => '0');
+   signal u1 : std_logic_vector(2 downto 0) := (others => '0');
+   signal u2 : std_logic_vector(2 downto 0) := (others => '0');
+   signal u3 : std_logic_vector(2 downto 0) := (others => '0');
 
-   signal f0 : std_logic_vector(2 downto 0);
-   signal f1 : std_logic_vector(2 downto 0);
-   signal f2 : std_logic_vector(2 downto 0);
-   signal f3 : std_logic_vector(2 downto 0);
+   signal f0 : std_logic_vector(2 downto 0) := (others => '0');
+   signal f1 : std_logic_vector(2 downto 0) := (others => '0');
+   signal f2 : std_logic_vector(2 downto 0) := (others => '0');
+   signal f3 : std_logic_vector(2 downto 0) := (others => '0');
 
-   signal r0 : std_logic_vector(2 downto 0);
-   signal r1 : std_logic_vector(2 downto 0);
-   signal r2 : std_logic_vector(2 downto 0);
-   signal r3 : std_logic_vector(2 downto 0);
+   signal r0 : std_logic_vector(2 downto 0) := (others => '0');
+   signal r1 : std_logic_vector(2 downto 0) := (others => '0');
+   signal r2 : std_logic_vector(2 downto 0) := (others => '0');
+   signal r3 : std_logic_vector(2 downto 0) := (others => '0');
 
-   signal d0 : std_logic_vector(2 downto 0);
-   signal d1 : std_logic_vector(2 downto 0);
-   signal d2 : std_logic_vector(2 downto 0);
-   signal d3 : std_logic_vector(2 downto 0);
+   signal d0 : std_logic_vector(2 downto 0) := (others => '0');
+   signal d1 : std_logic_vector(2 downto 0) := (others => '0');
+   signal d2 : std_logic_vector(2 downto 0) := (others => '0');
+   signal d3 : std_logic_vector(2 downto 0) := (others => '0');
 
-   signal b0 : std_logic_vector(2 downto 0);
-   signal b1 : std_logic_vector(2 downto 0);
-   signal b2 : std_logic_vector(2 downto 0);
-   signal b3 : std_logic_vector(2 downto 0);
+   signal b0 : std_logic_vector(2 downto 0) := (others => '0');
+   signal b1 : std_logic_vector(2 downto 0) := (others => '0');
+   signal b2 : std_logic_vector(2 downto 0) := (others => '0');
+   signal b3 : std_logic_vector(2 downto 0) := (others => '0');
 
-   signal l0 : std_logic_vector(2 downto 0);
-   signal l1 : std_logic_vector(2 downto 0);
-   signal l2 : std_logic_vector(2 downto 0);
-   signal l3 : std_logic_vector(2 downto 0);
+   signal l0 : std_logic_vector(2 downto 0) := (others => '0');
+   signal l1 : std_logic_vector(2 downto 0) := (others => '0');
+   signal l2 : std_logic_vector(2 downto 0) := (others => '0');
+   signal l3 : std_logic_vector(2 downto 0) := (others => '0');
 
 begin
 
@@ -95,26 +121,21 @@ begin
    begin
       if rising_edge(clk_i) then
          case cmd_i is
+
+--         U0 U1
+--         U2 U3
+--         -----
+-- L0 L1 | F0 F1 | R0 R1 | B0 B1
+-- L2 L3 | F2 F3 | R2 R3 | B2 B3
+--         -----
+--         D0 D1
+--         D2 D3
+
             when "0101" => -- : F+
                f1 <= f0;
                f3 <= f1;
                f2 <= f3;
                f0 <= f2;
-               --
-               r2 <= r0;
-               d1 <= r2;
-               d0 <= d1;
-               l3 <= d0;
-               l1 <= l3;
-               u2 <= l1;
-               u3 <= u2;
-               r0 <= u3;
-
-            when "0110" => -- : F2
-               f3 <= f0;
-               f0 <= f3;
-               f2 <= f1;
-               f1 <= f2;
                --
                d1 <= r0;
                l3 <= d1;
@@ -125,41 +146,52 @@ begin
                u3 <= l1;
                r2 <= u3;
 
+            when "0110" => -- : F2
+               f3 <= f0;
+               f0 <= f3;
+               f2 <= f1;
+               f1 <= f2;
+               --
+               l3 <= r0;
+               r0 <= l3;
+               u2 <= d1;
+               d1 <= u2;
+               l1 <= r2;
+               r2 <= l1;
+               d0 <= u3;
+               u3 <= d0;
+
             when "0111" => -- : F-
-               f2 <= d0;
+               f2 <= f0;
                f3 <= f2;
                f1 <= f3;
                f0 <= f1;
                --
-               u3 <= r0;
-               u2 <= u3;
-               l1 <= u2;
-               l3 <= l1;
-               d0 <= l3;
-               d1 <= d0;
-               r2 <= d1;
-               r0 <= r2;
+               r0 <= d1;
+               d1 <= l3;
+               l3 <= u2;
+               u2 <= r0;
+
+               r2 <= d0;
+               d0 <= l1;
+               l1 <= u3;
+               u3 <= r2;
+
+
+--         U2 U0
+--         U3 U1
+--         -----
+-- F0 F1 | R0 R1 | B0 B1 | L0 L1
+-- F2 F3 | R2 R3 | B2 B3 | L2 L3
+--         -----
+--         D1 D3
+--         D0 D2
 
             when "1001" => -- : R+
                r1 <= r0;
                r3 <= r1;
                r2 <= r3;
                r0 <= r2;
-               --
-               b2 <= b0;
-               d3 <= b2;
-               d1 <= d3;
-               f3 <= d1;
-               f1 <= f3;
-               u3 <= f1;
-               u1 <= u3;
-               b0 <= u1;
-
-            when "1010" => -- : R2
-               r0 <= r3;
-               r3 <= r0;
-               r1 <= r2;
-               r2 <= r1;
                --
                d3 <= b0;
                f3 <= d3;
@@ -170,41 +202,51 @@ begin
                u1 <= f1;
                b2 <= u1;
 
+            when "1010" => -- : R2
+               r0 <= r3;
+               r3 <= r0;
+               r1 <= r2;
+               r2 <= r1;
+               --
+               f3 <= b0;
+               b0 <= f3;
+               u3 <= d3;
+               d3 <= u3;
+               b2 <= f1;
+               f1 <= b2;
+               d1 <= u1;
+               u1 <= d1;
+
             when "1011" => -- : R-
                r2 <= r0;
                r3 <= r2;
                r1 <= r3;
                r0 <= r1;
                --
-               u1 <= b0;
-               u3 <= u1;
-               f1 <= u3;
-               f3 <= f1;
-               d1 <= f3;
-               d3 <= d1;
-               b2 <= d3;
-               b0 <= b2;
+               b0 <= d3;
+               d3 <= f3;
+               f3 <= u3;
+               u3 <= b0;
+               b2 <= d1;
+               d1 <= f1;
+               f1 <= u1;
+               u1 <= b2;
+
+
+--         B3 B2
+--         B1 B0
+--         -----
+-- L2 L0 | U0 U1 | R1 R3 | D3 D2
+-- L3 L1 | U2 U3 | R0 R2 | D1 D0
+--         -----
+--         F0 F1
+--         F2 F3
 
             when "1101" => -- : U+
                u1 <= u0;
                u3 <= u1;
                u2 <= u3;
                u0 <= u2;
-               --
-               r0 <= r1;
-               f1 <= r0;
-               f0 <= f1;
-               l1 <= f0;
-               l0 <= l1;
-               b1 <= l0;
-               b0 <= b1;
-               r1 <= b0;
-
-            when "1110" => -- : U2
-               u3 <= u0;
-               u0 <= u3;
-               u2 <= u1;
-               u1 <= u2;
                --
                f1 <= r1;
                l1 <= f1;
@@ -215,54 +257,69 @@ begin
                b0 <= l0;
                r0 <= b0;
 
+            when "1110" => -- : U2
+               u3 <= u0;
+               u0 <= u3;
+               u2 <= u1;
+               u1 <= u2;
+               --
+               l1 <= r1;
+               r1 <= l1;
+               f1 <= b1;
+               b1 <= f1;
+               r0 <= l0;
+               l0 <= r0;
+               f0 <= b0;
+               b0 <= f0;
+
             when "1111" => -- : U-
                u0 <= u1;
                u2 <= u0;
                u3 <= u2;
                u1 <= u3;
                --
-               r1 <= r0;
-               b0 <= r1;
-               b1 <= b0;
-               l0 <= b1;
-               l1 <= l0;
-               f0 <= l1;
-               f1 <= f0;
-               r0 <= f1;
+               r1 <= f1;
+               f1 <= l1;
+               l1 <= b1;
+               b1 <= r1;
+               r0 <= f0;
+               f0 <= l0;
+               l0 <= b0;
+               b0 <= r0;
 
             when others => null;
          end case;
 
          if rst_i = '1' then
-            u0 <= "000";
+            u0 <= "001";
             u1 <= "001";
-            u2 <= "010";
-            u3 <= "011";
+            u2 <= "001";
+            u3 <= "001";
 
-            f0 <= "001";
+            f0 <= "010";
             f1 <= "010";
-            f2 <= "011";
-            f3 <= "100";
+            f2 <= "010";
+            f3 <= "010";
 
-            r0 <= "010";
-            r1 <= "011";
-            r2 <= "100";
-            r3 <= "101";
+            r0 <= "000";
+            r1 <= "000";
+            r2 <= "000";
+            r3 <= "000";
 
             d0 <= "011";
-            d1 <= "100";
-            d2 <= "101";
-            d3 <= "000";
+            d1 <= "011";
+            d2 <= "011";
+            d3 <= "011";
 
             b0 <= "100";
-            b1 <= "101";
-            b2 <= "000";
-            b3 <= "001";
+            b1 <= "100";
+            b2 <= "100";
+            b3 <= "100";
 
             l0 <= "101";
-            l1 <= "000";
-            l2 <= "001";
-            l3 <= "010";
+            l1 <= "101";
+            l2 <= "101";
+            l3 <= "101";
          end if;
       end if;
    end process;
@@ -274,6 +331,31 @@ begin
              d0 = "011" and d1 = "011" and d2 = "011" and d3 = "011" and
              b0 = "100" and b1 = "100" and b2 = "100" and b3 = "100" and
              l0 = "101" and l1 = "101" and l2 = "101" and l3 = "101" else '0';
+
+   u0_o <= u0;
+   u1_o <= u1;
+   u2_o <= u2;
+   u3_o <= u3;
+   f0_o <= f0;
+   f1_o <= f1;
+   f2_o <= f2;
+   f3_o <= f3;
+   r0_o <= r0;
+   r1_o <= r1;
+   r2_o <= r2;
+   r3_o <= r3;
+   d0_o <= d0;
+   d1_o <= d1;
+   d2_o <= d2;
+   d3_o <= d3;
+   b0_o <= b0;
+   b1_o <= b1;
+   b2_o <= b2;
+   b3_o <= b3;
+   l0_o <= l0;
+   l1_o <= l1;
+   l2_o <= l2;
+   l3_o <= l3;
 
 end architecture synthesis;
 
--- rubik/rubik_formal.sv
+++ rubik/rubik_formal.sv
@@ -7,16 +7,35 @@ module rubik_formal(
    input  wire       clk_i,
    input  wire       rst_i,
    input  wire [3:0] cmd_i,
-   output wire       done_o);
+   output wire [2:0] u0_o,
+   output wire [2:0] u1_o,
+   output wire [2:0] u2_o,
+   output wire [2:0] u3_o,
+   output wire [2:0] f0_o,
+   output wire [2:0] f1_o,
+   output wire [2:0] f2_o,
+   output wire [2:0] f3_o,
+   output wire [2:0] r0_o,
+   output wire [2:0] r1_o,
+   output wire [2:0] r2_o,
+   output wire [2:0] r3_o,
+   output wire [2:0] d0_o,
+   output wire [2:0] d1_o,
+   output wire [2:0] d2_o,
+   output wire [2:0] d3_o,
+   output wire [2:0] b0_o,
+   output wire [2:0] b1_o,
+   output wire [2:0] b2_o,
+   output wire [2:0] b3_o,
+   output wire [2:0] l0_o,
+   output wire [2:0] l1_o,
+   output wire [2:0] l2_o,
+   output wire [2:0] l3_o,
+   output wire       done_o
+);
 
    // Instantiate the FETCH module
    rubik DUT (.*); // This only works as long as the port signal names are unchanged.
-
-   // This is necessary, because the $past() function returns uninitialized value on the first clock cycle.
-   reg f_past_valid;
-   initial f_past_valid = 1'b0;
-   always @(posedge clk_i)
-      f_past_valid <= 1'b1;
 
 
    /****************************
@@ -26,6 +45,92 @@ module rubik_formal(
    // Require reset at startup.
    // This is to ensure BMC starts in a valid state.
    initial assume(rst_i);
+
+
+   /****************************
+    * ASSERTIONS ABOUT OUTPUTS
+    ****************************/
+
+   always @(posedge clk_i)
+   begin
+      if (!rst_i)
+      begin
+         assert (u2_o != f0_o);
+         assert (u3_o != f1_o);
+         assert (l1_o != f0_o);
+         assert (l3_o != f2_o);
+         assert (d0_o != f2_o);
+         assert (d1_o != f3_o);
+         assert (r2_o != f3_o);
+         assert (r0_o != f1_o);
+
+         assert (u2_o != l1_o);
+         assert (u0_o != l0_o);
+         assert (l3_o != d0_o);
+         assert (l2_o != d2_o);
+         assert (d1_o != r2_o);
+         assert (d3_o != r3_o);
+         assert (r0_o != u3_o);
+         assert (r1_o != u1_o);
+
+         assert (r1_o != b0_o);
+         assert (r3_o != b2_o);
+         assert (d3_o != b2_o);
+         assert (d2_o != b3_o);
+         assert (u0_o != b1_o);
+         assert (u1_o != b0_o);
+         assert (b1_o != l0_o);
+         assert (b3_o != l2_o);
+      end
+   end
+
+   wire integer num_colors[0:5];
+   wire integer i;
+
+   always @(*)
+   begin
+      for (i=0; i<6; i=i+1)
+      begin
+         num_colors[i] = 0;
+      end
+      num_colors[u0_o] = num_colors[u0_o] + 1;
+      num_colors[u1_o] = num_colors[u1_o] + 1;
+      num_colors[u2_o] = num_colors[u2_o] + 1;
+      num_colors[u3_o] = num_colors[u3_o] + 1;
+      num_colors[f0_o] = num_colors[f0_o] + 1;
+      num_colors[f1_o] = num_colors[f1_o] + 1;
+      num_colors[f2_o] = num_colors[f2_o] + 1;
+      num_colors[f3_o] = num_colors[f3_o] + 1;
+      num_colors[r0_o] = num_colors[r0_o] + 1;
+      num_colors[r1_o] = num_colors[r1_o] + 1;
+      num_colors[r2_o] = num_colors[r2_o] + 1;
+      num_colors[r3_o] = num_colors[r3_o] + 1;
+      num_colors[d0_o] = num_colors[d0_o] + 1;
+      num_colors[d1_o] = num_colors[d1_o] + 1;
+      num_colors[d2_o] = num_colors[d2_o] + 1;
+      num_colors[d3_o] = num_colors[d3_o] + 1;
+      num_colors[b0_o] = num_colors[b0_o] + 1;
+      num_colors[b1_o] = num_colors[b1_o] + 1;
+      num_colors[b2_o] = num_colors[b2_o] + 1;
+      num_colors[b3_o] = num_colors[b3_o] + 1;
+      num_colors[l0_o] = num_colors[l0_o] + 1;
+      num_colors[l1_o] = num_colors[l1_o] + 1;
+      num_colors[l2_o] = num_colors[l2_o] + 1;
+      num_colors[l3_o] = num_colors[l3_o] + 1;
+   end
+
+   always @(posedge clk_i)
+   begin
+      if (!rst_i)
+      begin
+         assert (num_colors[0] == 4);
+         assert (num_colors[1] == 4);
+         assert (num_colors[2] == 4);
+         assert (num_colors[3] == 4);
+         assert (num_colors[4] == 4);
+         assert (num_colors[5] == 4);
+      end
+   end
 
 
    /********************
