24276bf5921ab9978945791622e5aa7fcf7b6c06
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2021-01-03 09:13:03 +0100

    two_stage_fifo: Ready is low after reset

--- two_stage_fifo/two_stage_fifo.vhd
+++ two_stage_fifo/two_stage_fifo.vhd
@@ -1,6 +1,8 @@
+-- An elastic pipeline with two stages. I.e. can accept two writes before blocking.
+-- In other words, a FIFO of depth two.
+
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std_unsigned.all;
 
 entity two_stage_fifo is
    generic (
@@ -25,15 +27,15 @@ architecture synthesis of two_stage_fifo is
    signal s_data_r  : std_logic_vector(G_DATA_SIZE-1 downto 0);
 
    -- Output registers
-   signal m_data_r  : std_logic_vector(G_DATA_SIZE-1 downto 0)   := (others => '0');
+   signal m_data_r  : std_logic_vector(G_DATA_SIZE-1 downto 0) := (others => '0');
 
    -- Control signals
    signal s_afull_r : std_logic := '0';
-   signal s_ready_r : std_logic := '1';
+   signal s_ready_r : std_logic := '0';
    signal m_valid_r : std_logic := '0';
 
-   type state_t is (EMPTY_ST, ONE_ST, TWO_ST);
-   signal state_r : state_t := EMPTY_ST;
+   type state_t is (EMPTY_ST, ONE_ST, TWO_ST, RESET_ST);
+   signal state_r : state_t := RESET_ST;
 
 begin
 
@@ -46,15 +48,6 @@ begin
       if rising_edge(clk_i) then
          case state_r is
             when EMPTY_ST =>
-               assert s_afull_r = '0'
-                  report "s_afull_r is not zero"
-                     severity error;
-
-               assert m_valid_r = '0'
-                  report "m_valid_r is not zero"
-                     severity error;
-
-               s_ready_r <= '1';
                if s_valid_i = '1' then
                   -- Ready is already asserted, so we have to accept the data
                   m_data_r  <= s_data_i;
@@ -66,16 +59,6 @@ begin
 
             when ONE_ST =>
                -- The pipe has valid data in m_*
-               assert s_afull_r = '1'
-                  report "s_afull_r is not one"
-                     severity error;
-               assert s_ready_r = '1'
-                  report "s_ready_r is not one"
-                     severity error;
-               assert m_valid_r = '1'
-                  report "m_valid_r is not one"
-                     severity error;
-
                case std_logic_vector'(m_ready_i & s_valid_i) is
                   when "00" =>
                      null;
@@ -101,16 +84,6 @@ begin
 
             when TWO_ST =>
                -- The pipe has valid data in both s_* and m_*
-               assert s_afull_r = '1'
-                  report "s_afull_r is not one"
-                     severity error;
-               assert s_ready_r = '0'
-                  report "s_ready_r is not zero"
-                     severity error;
-               assert m_valid_r = '1'
-                  report "m_valid_r is not one"
-                     severity error;
-
                if m_ready_i = '1' then
                   -- Valid is asserted, so data has been accepted
                   m_data_r  <= s_data_r;
@@ -120,13 +93,18 @@ begin
                   state_r   <= ONE_ST;
                end if;
 
+            when RESET_ST =>
+               s_ready_r <= '1';
+               s_afull_r <= '0';
+               m_valid_r <= '0';
+               state_r   <= EMPTY_ST;
          end case;
 
          if rst_i = '1' then
             s_afull_r <= '0';
-            s_ready_r <= '1';
+            s_ready_r <= '0';
             m_valid_r <= '0';
-            state_r   <= EMPTY_ST;
+            state_r   <= RESET_ST;
          end if;
       end if;
    end process p_fsm;
