7f3cb5e51c77450da37396a519eb4b56a4ed0b7d
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2021-01-03 21:19:39 +0100

    fetch2: Add more verification

--- fetch2/fetch.vhd
+++ fetch2/fetch.vhd
@@ -12,8 +12,8 @@ use ieee.numeric_std_unsigned.all;
 -- The wishbone interface is running in pipeline mode. This means the STB
 -- signal is asserted for one clock cycle (or until STALL is low) for each
 -- request, whereas the CYC signal is held high until the corresponding ACKs
--- are received.  In this implementation, only a single outstanding wishbone
--- request is used.
+-- are received.  In this implementation, up to two outstanding wishbone
+-- request are used.
 
 entity fetch is
    port (
@@ -54,8 +54,6 @@ architecture synthesis of fetch is
    signal osb_out_data_valid : std_logic;
    signal osb_out_data_ready : std_logic;
    signal osb_out_data_data  : std_logic_vector(15 downto 0);
-
-   signal dc_addr_data       : std_logic_vector(31 downto 0);
 
 begin
 
@@ -106,38 +104,42 @@ begin
    end process p_wishbone;
 
 
+   -- FIFO to store the WISHBONE address
    i_two_stage_fifo_addr : entity work.two_stage_fifo
       generic map (
          G_DATA_SIZE => 16
       )
       port map (
          clk_i     => clk_i,
-         rst_i     => rst_i,
+         rst_i     => rst_i or dc_valid_i,
          s_valid_i => wb_cyc_o and wb_stb_o,
          s_ready_o => tsf_in_addr_ready,
          s_data_i  => wb_addr_o,
          s_afull_o => tsf_in_addr_afull,
          m_valid_o => tsf_out_addr_valid,
-         m_ready_i => tsf_out_addr_ready or dc_valid_i,
+         m_ready_i => tsf_out_addr_ready,
          m_data_o  => tsf_out_addr_data
       ); -- i_two_stage_fifo_addr
 
 
+   -- FIFO to store the WISHBONE data
    i_one_stage_buffer_data : entity work.one_stage_buffer
       generic map (
          G_DATA_SIZE => 16
       )
       port map (
          clk_i     => clk_i,
-         rst_i     => rst_i,
+         rst_i     => rst_i or dc_valid_i,
          s_valid_i => wb_cyc_o and wb_ack_i,
          s_ready_o => osb_in_data_ready,
          s_data_i  => wb_data_i,
          m_valid_o => osb_out_data_valid,
-         m_ready_i => osb_out_data_ready or dc_valid_i,
+         m_ready_i => osb_out_data_ready,
          m_data_o  => osb_out_data_data
       ); -- i_one_stage_buffer_data
 
+
+   -- Concatenate WISHBONE address and data
    i_pipe_concat : entity work.pipe_concat
       generic map (
          G_DATA0_SIZE => 16,
@@ -154,11 +156,9 @@ begin
          s0_data_i  => osb_out_data_data,
          m_valid_o  => dc_valid_o,
          m_ready_i  => dc_ready_i,
-         m_data_o   => dc_addr_data
+         m_data_o(31 downto 16) => dc_addr_o,
+         m_data_o(15 downto 0)  => dc_data_o
       ); -- i_pipe_concat
-
-   dc_addr_o <= dc_addr_data(31 downto 16);
-   dc_data_o <= dc_addr_data(15 downto 0);
 
 end architecture synthesis;
 
