257e2dc2e70a3a60e4148f2ed686c859fab4a5f0
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2021-02-04 08:09:49 +0100

    cpu: Simplify logic

--- cpu/execute.vhd
+++ cpu/execute.vhd
@@ -63,10 +63,10 @@ architecture synthesis of execute is
    constant C_MEM_WRITE    : integer := 1;
    constant C_REG_WRITE    : integer := 0;
 
+   -- Bypass logic
    signal reg_we_d      : std_logic;
    signal reg_addr_d    : std_logic_vector(3 downto 0);
    signal reg_val_d     : std_logic_vector(15 downto 0);
-
    signal dec_src_val   : std_logic_vector(15 downto 0);
    signal dec_dst_val   : std_logic_vector(15 downto 0);
 
@@ -84,14 +84,21 @@ architecture synthesis of execute is
 
 begin
 
+   dec_ready_o <= not (wait_for_mem_src or wait_for_mem_dst or wait_for_mem_access);
+
    wait_for_mem_src    <= dec_valid_i and dec_microop_i(C_MEM_ALU_SRC) and not mem_src_valid_i;
    wait_for_mem_dst    <= dec_valid_i and dec_microop_i(C_MEM_ALU_DST) and not mem_dst_valid_i;
    wait_for_mem_access <= dec_valid_i and (dec_microop_i(C_MEM_ALU_DST) or
                                            dec_microop_i(C_MEM_READ_SRC) or
                                            dec_microop_i(C_MEM_READ_DST)) and not mem_ready_i;
 
-   alu_oper       <= dec_opcode_i;
-   alu_flags      <= dec_flags_i;
+   mem_src_ready_o <= dec_microop_i(C_MEM_ALU_SRC);
+   mem_dst_ready_o <= dec_microop_i(C_MEM_ALU_DST);
+
+
+   ------------------------------------------------------------
+   -- Bypass logic
+   ------------------------------------------------------------
 
    p_delay : process (clk_i)
    begin
@@ -102,22 +109,20 @@ begin
       end if;
    end process p_delay;
 
-   dec_src_val    <= reg_val_d when reg_we_d = '1' and reg_addr_d = dec_src_addr_i else
-                     reg_val_o when reg_we_o = '1' and reg_addr_o = dec_src_addr_i else
-                     dec_src_val_i;
+   dec_src_val <= reg_val_d when reg_we_d = '1' and reg_addr_d = dec_src_addr_i else
+                  dec_src_val_i;
+   dec_dst_val <= reg_val_d when reg_we_d = '1' and reg_addr_d = dec_dst_addr_i else
+                  dec_dst_val_i;
 
-   dec_dst_val    <= reg_val_d when reg_we_d = '1' and reg_addr_d = dec_dst_addr_i else
-                     reg_val_o when reg_we_o = '1' and reg_addr_o = dec_dst_addr_i else
-                     dec_dst_val_i;
 
-   alu_src_val    <= mem_src_data_i when dec_microop_i(C_MEM_ALU_SRC) = '1' else dec_src_val;
-   alu_dst_val    <= mem_dst_data_i when dec_microop_i(C_MEM_ALU_DST) = '1' else dec_dst_val;
+   ------------------------------------------------------------
+   -- ALU
+   ------------------------------------------------------------
 
-   dec_ready_o    <= not (wait_for_mem_src or wait_for_mem_dst or wait_for_mem_access);
-
-   mem_src_ready_o <= dec_microop_i(C_MEM_ALU_SRC);
-   mem_dst_ready_o <= dec_microop_i(C_MEM_ALU_DST);
-
+   alu_oper    <= dec_opcode_i;
+   alu_flags   <= dec_flags_i;
+   alu_src_val <= mem_src_data_i when dec_microop_i(C_MEM_ALU_SRC) = '1' else dec_src_val;
+   alu_dst_val <= mem_dst_data_i when dec_microop_i(C_MEM_ALU_DST) = '1' else dec_dst_val;
 
    i_alu_data : entity work.alu_data
       port map (
@@ -143,25 +148,12 @@ begin
       ); -- i_alu_flags
 
 
-   p_reg : process (clk_i)
-   begin
-      if rising_edge(clk_i) then
-         reg_we_o       <= '0';
-         reg_addr_o     <= (others => '0');
-         reg_val_o      <= (others => '0');
-         reg_flags_o    <= alu_res_flags;
-         reg_flags_we_o <= dec_flags_we_i and dec_valid_i and dec_ready_o;
+   reg_flags_o    <= alu_res_flags;
+   reg_flags_we_o <= dec_flags_we_i and dec_valid_i and dec_ready_o;
 
-         if dec_valid_i = '1' and dec_ready_o = '1' then
-            if dec_microop_i(C_REG_WRITE) = '1' then
-               reg_we_o   <= '1';
-               reg_addr_o <= dec_reg_addr_i;
-               reg_val_o  <= alu_res_data(15 downto 0);
-            end if;
-         end if;
-      end if;
-   end process p_reg;
-
+   reg_addr_o     <= dec_reg_addr_i;
+   reg_val_o      <= alu_res_data(15 downto 0);
+   reg_we_o       <= dec_valid_i and dec_ready_o and dec_microop_i(C_REG_WRITE);
 
    mem_valid_o    <= dec_valid_i and not (wait_for_mem_src or wait_for_mem_dst) and or(dec_microop_i(3 downto 1));
    mem_op_o       <= dec_microop_i(3 downto 1);
